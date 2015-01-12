914b3640a7938f490bd0357ea47cf2c029371f42
psolin <paul@paulsolin.com>
2015-01-12 14:29:54 -0500

    conlficted copies

--- test2 (localhost's conflicted copy 2015-01-11).py
+++
@@ -1,9 +0,0 @@
-from cleanco import cleanco
-
-business_name = "Cleanco mfg ses."
-test = cleanco(business_name)
-
-print business_name
-print test.clean_name()
-print test.country()
-print test.type()
