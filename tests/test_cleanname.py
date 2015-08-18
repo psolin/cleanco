5d6ab997bf86daaa3752e824c845aaefe9ec8384
Petri Savolainen <petri.savolainen@koodaamo.fi>
2015-08-18 16:42:02 +0300

    implement #8

--- tests/test_cleanname.py
+++ tests/test_cleanname.py
@@ -4,9 +4,12 @@ from cleanco import cleanco
 # Tests that demonstrate stuff is stripped away
 
 basic_cleanup_tests = {
-   "name with suffix":    "Hello World Oy",
-   "name w/ ', ltd.'":    "Hello World, ltd.",
-   "name with extra ws":  "Hello    World ltd",
+   "name w/ suffix":           "Hello World Oy",
+   "name w/ ', ltd.'":         "Hello World, ltd.",
+   "name w/ ws suffix ws":     "Hello    World ltd",
+   "name w/ suffix ws":        "Hello World ltd ",
+   "name w/ suffix dot ws":    "Hello World ltd. ",
+   "name w/ ws suffix dot ws": " Hello World ltd. ",
 }
 
 def test_basic_cleanups():
@@ -14,6 +17,22 @@ def test_basic_cleanups():
    errmsg = "cleanup of %s failed"
    for testname, variation in basic_cleanup_tests.items():
       assert cleanco(variation).clean_name() == expected, errmsg % testname
+
+multi_cleanup_tests = {
+   "name + suffix":          "Hello World Oy",
+   "name + two suffix":      "Hello World Ab Oy",
+   "prefix + name":          "Oy Hello World",
+   "prefix + name + suffix": "Oy Hello World Ab",
+   "name w/ term in middle": "Hello Oy World",
+   "name w/ mid + suffix":   "Hello Oy World Ab"
+}
+
+def test_multi_type_cleanups():
+   expected = "Hello World"
+   errmsg = "cleanup of %s failed"
+   for testname, variation in multi_cleanup_tests.items():
+      result = cleanco(variation).clean_name(prefix=True, suffix=True, middle=True, multi=True)
+      assert result == expected, errmsg % testname
 
 
 # Tests that demonstrate organization name is kept intact
