27ab1404685312a09d07409e25875bec057fcbbf
Petri Savolainen <petri.savolainen@koodaamo.fi>
2015-08-18 13:24:26 +0300

    add some Trove classifiers

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from distutils.core import setup
 setup(name='cleanco',
       description='Python library to process company names',
       version='1.0',
+      classifiers = [
+         "Topic :: Office/Business",
+         "Development Status :: 4 - Beta",
+         "Intended Audience :: Developers"
+      ],
       url='https://github.com/psolin/cleanco',
       author='Paul Solin',
       author_email='paul@paulsolin.com',
