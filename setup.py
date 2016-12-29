cbb7f46101bf1c0d9b8814dd6f7cf23f48e0ac05
Petri Savolainen <petri.savolainen@koodaamo.fi>
2016-12-29 09:49:37 +0200

    add trove classifiers for Python 2.7 & 3.5 (for which tox tests pass)

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(name='cleanco',
          "Development Status :: 4 - Beta",
          "Intended Audience :: Developers",
          "License :: OSI Approved :: MIT License",
+         "Programming Language :: Python :: 2.7",
+         "Programming Language :: Python :: 3.5"
       ],
       url='https://github.com/psolin/cleanco',
       author='Paul Solin',
