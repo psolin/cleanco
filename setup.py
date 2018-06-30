63fa4466d867bd4b573755325901718cf7180cc3
JHFVR <36917584+JHFVR@users.noreply.github.com>
2018-06-30 19:59:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(name='cleanco',
       description='Python library to process company names',
-      version='1.36',
+      version='1.361',
       license="MIT",
       classifiers = [
          "Topic :: Office/Business",
