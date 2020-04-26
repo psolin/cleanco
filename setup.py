6dd6168bfbfc93139bc0b19a5cbee8c9330956a0
Petri Savolainen <petri@koodaamo.fi>
2020-04-26 18:50:11 +0300

    load README into package description

--- setup.py
+++ setup.py
@@ -4,8 +4,13 @@ import setuptools
 
 from setuptools import setup
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='cleanco',
       description='Python library to process company names',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       version='2.0.0',
       license="MIT",
       classifiers = [
