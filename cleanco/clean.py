3199c28d49636352ec9f755d2e878e9c7eec02a4
taraskuzyk <taras@leadstheory.com>
2021-05-21 09:50:29 -0600

    made sure public API hasn't changed for prepare_terms being moved inside basename

--- cleanco/clean.py
+++ cleanco/clean.py
@@ -73,10 +73,10 @@ def prepare_terms():
     return [(len(tp), tp) for tp in sntermparts]
 
 
-def basename(name, suffix=True, prefix=False, middle=False, **kwargs):
+def basename(name, terms=None, suffix=True, prefix=False, middle=False, **kwargs):
     "return cleaned base version of the business name"
-
-    terms = prepare_terms()
+    if terms is None:
+        terms = prepare_terms()
     name = strip_tail(name)
     nparts = name.split()
     nname = normalized(name)
