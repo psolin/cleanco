806bf5f2439b96d8b5b5e21af824d5c870aba168
Petri Savolainen <petri@koodaamo.fi>
2021-11-16 21:34:47 +0200

    remove unused old test file

--- tests/test2.py
+++
@@ -1,8 +0,0 @@
-from cleanco import cleanco
-
-business_name = "Hello World, llc."
-print("Inputted Business Name: %s" % business_name)
-x = cleanco(business_name)
-print("Clean Name: %s" % x.clean_name())
-print("Business Type: %s" % x.type())
-print("Country: %s" % x.country())
